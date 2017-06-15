cd4f9f9e4b19ac0909a87e829fd10cbe01b1c84f
Jonburned <jonburned@gmail.com>
2017-06-15 21:59:42 +0300

    changes in category action

--- src/Crowd/FoundingBundle/Controller/PageController.php
+++ src/Crowd/FoundingBundle/Controller/PageController.php
@@ -20,13 +20,32 @@ class PageController extends Controller
         return $this->render('CrowdFoundingBundle:Page:index.html.twig', array('categories' => $categories, 'amountProjects' => $amountProjects));
     }
 
-    public function categoryAction()
+    public function categoryAction($page)
     {
-        return $this->render('CrowdFoundingBundle:Page:category.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        if (strcasecmp($page, 'all') == 0)
+        {
+            $projects = $em->getRepository('CrowdFoundingBundle:Project')->findAll();
+        }
+        else
+        {
+            $id = $em->getRepository('CrowdFoundingBundle:Category')->findOneBy(array('page' => $page));
+            $projects = $em->getRepository('CrowdFoundingBundle:Project')->findBy(array('category' => $id));
+        }
+        $categories = $em->getRepository('CrowdFoundingBundle:Category')->getAll();
+        $amountProjects = 0;
+        foreach ($categories as $category)
+        {
+            $amountProjects += count($category->getProjects());
+        }
+        return $this->render('CrowdFoundingBundle:Page:category.html.twig', array('projects' => $projects, 'page' => $page, 'categories' => $categories, 'amountProjects' => $amountProjects));
     }
 
     public function projectAction()
     {
+
+
+
         return $this->render('CrowdFoundingBundle:Page:project.html.twig');
     }
 }
